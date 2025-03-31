aa752761a93dc6136c0d928d3856310389f706ac
Roni Seppälä <roni.o.seppala@gmail.com>
2025-03-31 18:34:33 +0300

    added message loging for testing purposes

--- backend/src/routes/ai.ts
+++ backend/src/routes/ai.ts
@@ -2,6 +2,7 @@ import { Router, Request, Response } from 'express';
 import axios from 'axios';
 import OpenAI from "openai"
 import dotenv from 'dotenv';
+import fs from 'fs';
 dotenv.config();
 
 const router: Router = Router();
@@ -15,6 +16,27 @@ router.post('/call', async (req: Request, res: Response) => {
 
     console.log(req.body.text);
     console.log(req.body.history);
+
+    //check if log file exists
+    const filePath = 'logs/conversation.log';
+
+    if (fs.existsSync(filePath)) {
+        console.log('File exists.');
+    } else {
+        console.log('File does not exist.');
+        // Optionally, create the file
+        fs.writeFileSync(filePath, '', 'utf8');
+    }
+
+    //log request text
+    const userLogEntry = `${new Date().toISOString()} - User: ${req.body.text}\n`;
+    fs.appendFile(filePath, userLogEntry, (err) => {
+        if (err) {
+            console.error('Error writing to log file', err);
+        } else {
+            console.log('Log entry added');
+        }
+    });
 
     const previousMessages = history.map((message: { sender: string; text: string }) => {
         return { role: message.sender === 'User' ? 'user' : 'assistant', content: message.text };
@@ -37,6 +59,16 @@ router.post('/call', async (req: Request, res: Response) => {
         res.status(500).json({ response: 'An error occurred. Please try again.' });
         return;
     }
+
+    //log ai response
+    const aiLogEntry = `${new Date().toISOString()} - AI : ${aiMessage}\n`;
+    fs.appendFile(filePath, aiLogEntry, (err) => {
+        if (err) {
+            console.error('Error writing to log file', err);
+        } else {
+            console.log('Log entry added');
+        }
+    });
 
     const response = `Response for: "${text} from backend"`;
     res.json({ response, aiMessage });
